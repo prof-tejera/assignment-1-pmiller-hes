1c580614c8bde8be1d08075637f4b7d016e78168
pmiller-hes <pmiller-hes@users.noreply.github.com>
2023-10-29 09:50:26 +0000

    deploy: b76595013dcf40fcb70463e9a4a8806e55a5fb60

--- src/components/generic/Input/Input.js
+++
@@ -1,7 +0,0 @@
-import "./Input.css";
-
-const Input = ({ className, placeHolder, ...props })  => {
-  return <span><input className={'default-inputField '+ className} placeholder={placeHolder} {...props} /></span>;
-};
-
-export default Input;
--- src/components/generic/Panel/Panel.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-
-
-const Panel =  ({ className, ...props })  => {
-  return <div className={'default-panel ' + className}>{props.children}</div>;
-};
-
-export default Panel;
